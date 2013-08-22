260691eeb9776e4c8dc3deca0a42a38b60adfaf3
jcarrharris <joncarrharris@gmail.com>
2013-08-22 11:11:46 -0400

    Minor edits

---
+++ Day 4_Ruby 3/animals/cat.rb
@@ -0,0 +1,11 @@
+require "./mammal"
+
+class Cat < Mammal
+	def speak
+		"meow, purrrrrrrr"
+	end
+
+	def move
+		"prowl silently on four paws"
+	end
+end
---
+++ Day 4_Ruby 3/animals/human.rb
@@ -0,0 +1,12 @@
+require "./mammal"
+
+class Human < Mammal
+	def speak
+		"Nice to meet you"
+	end
+	def move
+		"Walk and talk"
+	end
+	def reproduce
+		"Egg stays in"
+end
