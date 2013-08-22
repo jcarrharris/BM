1db9dd33187fa76930b7868672e99fdf13579564
jcarrharris <joncarrharris@gmail.com>
2013-08-22 10:35:24 -0400

    Added hangman game

--- Day 3_Ruby 2/hangman_3.rb
+++ Day 3_Ruby 2/hangman_3.rb
@@ -1,38 +1,52 @@
-class Game
-	def play_game
-		get_a_word
-		hide_the_word
-        while @word != @user_word
-		guess_the_word
-        match_the_letter
+# Hangman Game (Requires 'random-word' gem)
+class Hangman
+
+    require 'random-word'
+
+    def play_game
+        @lives = 8
+        puts "Welcome to Hangman!"
+        get_a_word
+        hide_the_word
+        while @word != @user_word && @lives > 0
+                guess_the_word
+                match_the_letter
         end
+        puts @lives > 0 ? "Congrats bro!" : "The correct word was #{@word}.\nGame Over Man!"
     end
 
     def get_a_word
-	   @word = "victory"
+        @word = RandomWord.adjs.next
     end
 
     def hide_the_word
-           @user_word = @word.gsub(/\w/,"_")
-           puts @user_word
+        @user_word = @word.gsub(/\w/, "_")
+        puts @user_word
     end
 
     def guess_the_word
-        puts "Guess a letter"
-	   @input = gets.chomp
+        puts "Please guess a (single) letter. You have #{@lives} lives left!"
+        @input = gets.chomp
     end
 
     def match_the_letter
         index = 0
-        @word.each_char do |l| 
-            if l == @input
-            @user_word[index] = @input
+        if @word.each_char.include?(@input)
+            puts "You got the right letter!"
+            @word.each_char do |l|
+                if l == @input
+                    @user_word[index] = @input
+                end
+                index += 1
             end
-            index += 1
+        else
+            puts "You got the wrong letter!"
+            @lives -= 1
         end
         puts @user_word
     end
+
 end
 
-game = Game.new
+game = Hangman.new
 game.play_game
