d58f62eef5496771f79708b78b1ee4a0a23d31bd
jcarrharris <joncarrharris@gmail.com>
2013-08-23 11:39:58 -0400

    fixed the bot

--- Day 5_Ruby 4/hello_bot.rb
+++ Day 5_Ruby 4/hello_bot.rb
@@ -9,25 +9,21 @@ greetings = ["hello", "hi", "hola", "yo", "wazup", "guten tag", "howdy", "saluta
 
 irc_server = TCPSocket.open(server, port)
 
-irc_server.puts "USER bhellobot = * BHelloBot"
+irc_server.puts "USER bhellobot 0 * BHelloBot"
 irc_server.puts "NICK #{nick}"
-irc_server.puts "JOin #{channel}"
+irc_server.puts "Join #{channel}"
 #irc_server.puts "PRIVMSG #{channel} :Hello from IRB Bot"
 
 # Hello, Bot!
-until irc_server.of?  do
-	msg = irc server.gets.downcase
+until irc_server.eof?  do
+	msg = irc_server.gets.downcase
 	puts msg
 
 	wasGreeted = false
 	greetings.each { |g| wasGreeted = true if msg.include? g}
 
-	if msg.include? greeting_prefix && wasGreeted
+	if msg.include? greeting_prefix and wasGreeted
 		response = "w00t! Someone talked to us!!! Hello!!"
 		irc_server.puts "PRIVMSG #{channel} :#{response}"
 	end
-end
-
-
-	
 end
