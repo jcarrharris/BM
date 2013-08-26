32b836ef7a033904d2d7078c7c8ff76bfdddc4b5
jcarrharris <joncarrharris@gmail.com>
2013-08-26 10:18:01 -0400

    added classes

--- Day 5_Ruby 4/hello_bot.rb
+++ Day 5_Ruby 4/hello_bot.rb
@@ -1,18 +1,40 @@
 require "socket"
+require 'rubygems'
+require 'nokogiri'
+require 'open-url'
+require 'weatherboy'
 
 server = "chat.freenode.net"
 port = "6667"
-nick = "HelloBot"
+nick = "FunnyBot"
 channel = "#bitmaker"
 greeting_prefix = "privmsg #bitmaker :"
 greetings = ["hello", "hi", "hola", "yo", "wazup", "guten tag", "howdy", "salutations", "who the hell are you"]
 
 irc_server = TCPSocket.open(server, port)
 
-irc_server.puts "USER bhellobot 0 * BHelloBot"
+irc_server.puts "USER bfunnybot 0 * BFunnyBot"
 irc_server.puts "NICK #{nick}"
 irc_server.puts "Join #{channel}"
 #irc_server.puts "PRIVMSG #{channel} :Hello from IRB Bot"
+
+#Weatherboy
+weatherboy = Weatherboy.new(90210)
+  weatherboy = Weatherboy.new("Indianapolis, IN")
+  weatherboy = Weatherboy.new
+
+  weatherboy.location = "New York, NY"
+    weatherboy.location = 46220
+    weatherboy.location
+    => 46220
+
+    w = weatherboy.current
+     => #<Weatherboy::Current:0x8c3dfe0...
+     w.weather
+     => "Overcast"
+     w.temp_f
+     => "31"
+
 
 # Hello, Bot!
 until irc_server.eof?  do
@@ -23,7 +45,7 @@ until irc_server.eof?  do
 	greetings.each { |g| wasGreeted = true if msg.include? g}
 
 	if msg.include? greeting_prefix and wasGreeted
-		response = "w00t! Someone talked to us!!! Hello!!"
+		response = "#{w}"
 		irc_server.puts "PRIVMSG #{channel} :#{response}"
 	end
 end
