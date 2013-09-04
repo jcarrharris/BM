b3224175cf088038bae6ee8e754a8367dea93779
jcarrharris <joncarrharris@gmail.com>
2013-09-04 11:21:28 -0400

    added posts

--- Day 12_Rails/blog/db/migrate/20130904142102_create_posts.rb
+++ Day 12_Rails/blog/db/migrate/20130904142102_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-    	t.string :title, null: false 
+    	t.string :title, :null => false 
     	t.text :body
 
       t.timestamps
